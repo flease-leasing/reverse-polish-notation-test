820cb1c2fc4b14931221abf0bdbe243ca315f2dd
E-Kalla <E-Kalla@elias.kalla-diouloufet@epitech.eu>
2021-03-13 11:41:42 +0100

    rpn calculator + add 1 unit test

--- src/test/rpn-calculator.spec.ts
+++ src/test/rpn-calculator.spec.ts
@@ -12,4 +12,10 @@ describe('Reverse polish notation from fixture 2', () => {
 
     expect(result).toEqual(1);
   });
+
+  it('should compute reverse polish notation from fixture 2', () => {
+    const result = evaluateRpn('1 2 + 4 * 3 +');
+
+    expect(result).toEqual(15);
+  });
 });
