eb928e0f625079f8b00818348da81f4d2fda1501
Dromd <dromdx00@gmail.com>
2021-03-13 19:33:59 +0100

    My answer

--- src/test/rpn-calculator.spec.ts
+++ src/test/rpn-calculator.spec.ts
@@ -12,4 +12,16 @@ describe('Reverse polish notation from fixture 2', () => {
 
     expect(result).toEqual(1);
   });
+
+  it('should compute reverse polish notation of numbers with multiple numeric', () => {
+    const result = evaluateRpn('36 100 10 / *');
+
+    expect(result).toEqual(360);
+  });
+
+  it('should compute reverse polish notation of numbers with decimals', () => {
+    const result = evaluateRpn('0.5 10 * 0.5 /');
+
+    expect(result).toEqual(10);
+  });
 });
